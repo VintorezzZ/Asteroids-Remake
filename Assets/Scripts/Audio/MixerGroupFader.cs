98f2501857d61120dd7bac26b14011bf56fddd43
VintorezzZ <sergeevmisha16@gmail.com>
2021-09-25 20:50:17 +0300

    Create audio mixer fader

--- Assets/Scripts/Audio/MixerGroupFader.cs
+++ Assets/Scripts/Audio/MixerGroupFader.cs
@@ -5,7 +5,7 @@
     using UnityEngine.Audio;
     using UnityEngine;
 
-    public static class FadeMixerGroup {
+    public static class MixerGroupFader {
 
         public static IEnumerator StartFade(AudioMixer audioMixer, string exposedParam, float duration, float targetVolume)
         {
@@ -17,12 +17,11 @@
 
             while (currentTime < duration)
             {
-                currentTime += Time.deltaTime;
+                currentTime += Time.unscaledDeltaTime;
                 float newVol = Mathf.Lerp(currentVol, targetValue, currentTime / duration);
                 audioMixer.SetFloat(exposedParam, Mathf.Log10(newVol) * 20);
                 yield return null;
             }
-            yield break;
         }
     }
 }
---
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.Audio;
+using EventHandler = DefaultNamespace.EventHandler;
+
+public class SoundManager : SingletonBehaviour<SoundManager>
+{
+    [SerializeField] private AudioSource musicSource, uiSource, inGameSource;
+    [SerializeField] private AudioMixer masterMixer;
+    public static float CurrentVolume => AudioListener.volume;
+    public float globalVolume = .5f;
+    private Coroutine _fadeCoroutine;
+    private void Awake()
+    {
+        InitializeSingleton();
+
+        AudioListener.volume = globalVolume;
+        
+        EventHandler.gamePaused += OnGamePaused;
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+        EventHandler.gamePaused -= OnGamePaused;
+    }
+
+    private void OnGamePaused(bool paused)
+    {
+        if(_fadeCoroutine != null) StopCoroutine(_fadeCoroutine);
+            
+        if (paused)
+        {
+            FadeMixerGroup(masterMixer, 0, duration: .5f);
+        }       
+        else
+        {
+            FadeMixerGroup(masterMixer, 1, duration: .5f);
+        }
+    }
+
+    private void FadeMixerGroup(AudioMixer audioMixer, float targetVolume, string exposedParam = "volume", float duration = 1f)
+    {
+        _fadeCoroutine = StartCoroutine(MixerGroupFader.StartFade(audioMixer, exposedParam, duration, targetVolume));
+    }
+
+    public void PlayBoomSFX(AudioClip audioClip)
+    {
+        inGameSource.PlayOneShot(audioClip);
+    }
+}
